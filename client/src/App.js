a02214953929c2b5a5b388490b5fc74efe199a0b
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-10-05 09:36:05 +0530

    AI

--- client/src/App.js
+++ client/src/App.js
@@ -56,3 +56,21 @@ const App = () => {
 };
 
 export default App;
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from '../components/Login';
+// import other components (Dashboard, etc.)
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />  {/* Protected Route */}
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
